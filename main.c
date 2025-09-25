1f226853fa10cb07a1caceff8e4965c7a10b2408
Fabricio <fabriciosilveira311@gmail.com>
2025-09-24 23:43:32 -0300

    Adicionado codigos
    atualização

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int fala_tu = 1;
+    int eae = 10;
+
+        while (fala_tu <= 8){
+            printf ("esse e o primeiro loop\n");
+            printf("%d",fala_tu);
+            printf("---\n");
+
+            fala_tu ++;
+    }
+        while (eae <= 30){
+
+            printf("esse e o segundo loop\n");
+            printf("%d",eae);
+            printf("---\n");
+
+            eae++;
+
+    }
+
+    return 0;
+}
